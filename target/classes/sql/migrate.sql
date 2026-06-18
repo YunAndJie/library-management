af0cf905a1be28be22e028d29cc65e6bd020bbc3
YunAndJie <1025699209@qq.com>
2026-06-18 10:15:16 +0800

    Delete target directory
    这是一个无用文件

--- target/classes/sql/migrate.sql
+++
@@ -1,8 +0,0 @@
--- 删除旧表（会丢失数据，仅用于开发环境）
-DROP TABLE IF EXISTS borrow_record;
-DROP TABLE IF EXISTS book;
-DROP TABLE IF EXISTS categroy;
-DROP TABLE IF EXISTS user;
-
--- 重新建表
-source library_init.sql;
